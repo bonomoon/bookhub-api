7d57a4265511e1fd88e640a99c4c72321ca9260c
Bong-oh Moon <bongohmoon@gmail.com>
2023-04-07 13:27:34 +0900

    feat: initialize bookhub api project

---
+++ src/test/java/dev/bonomoon/bookhubapi/BookhubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bonomoon.bookhubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookhubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
